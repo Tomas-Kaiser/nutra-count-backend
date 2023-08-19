3da8632e58e90299b35406459c6450a7a122545d
Tomas Kaiser <tomas.kaiser06@gmail.com>
2023-08-19 17:34:16 +0100

    Set up .env

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 import express from "express"
+// import dotenv from 'dotenv/config'
+// import 'dotenv/config'
+import { config } from 'dotenv';
+
 import productRouter from "./routers/products"
+
+// To load .env
+config()
+// To load specific .env.{environment}
+config({ path: `.env.${process.env.NODE_ENV}` });
+const PORT = process.env.PORT || 5001
 const app = express()
+
 
 // To parse json from client
 app.use(express.json())
-app.use("/products", productRouter)
+app.use("/api/products", productRouter)
 
 app.get("/", (req, res) => {
     res.send("Hello NutraCount :)")
 })
 
-app.listen(8000, () => console.log("Server listening on port 8000..."))
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`))
