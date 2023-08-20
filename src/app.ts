8df2775c78e1d1a252f33d5820cea1451701264e
Tomas Kaiser <tomas.kaiser06@gmail.com>
2023-08-20 21:58:39 +0100

    Connection to mongo DB compass

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,18 @@
-import express, { Request, Response, NextFunction, ErrorRequestHandler, Handler } from "express"
-// import dotenv from 'dotenv/config'
-// import 'dotenv/config'
+import express from "express"
 import { config } from 'dotenv';
 
 import productRouter from "./routers/products"
 import errorHandler from "./middleware/errorHandler";
+import connectDb from "./config/db";
 
 // To load .env
 config()
 // To load specific .env.{environment}
 config({ path: `.env.${process.env.NODE_ENV}` });
-const PORT = process.env.PORT || 5001
+
+connectDb()
+
 const app = express()
-
-
 // To parse json from client
 app.use(express.json())
 app.use("/api/products", productRouter)
@@ -24,4 +23,5 @@ app.get("/", (req, res) => {
 
 app.use(errorHandler);
 
+const PORT = process.env.PORT || 5001
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`))
