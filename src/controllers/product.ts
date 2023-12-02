cde77df39324dee8b822ffdb837b3b8b0a9f27c1
Tomas Kaiser <tomas.kaiser06@gmail.com>
2023-12-02 22:01:33 +0000

    Update product controller

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,61 +1,64 @@
 import { RequestHandler } from "express";
+
 import { Product } from "../models/Product";
 import ProductDto from "../dtos/Product";
-import ProductDTO from "../dtos/Product";
 
-const products: Product[] = []
 
-export const getProduct: RequestHandler = (req, res, next) => {
-    res.json({ products })
+export const getProducts: RequestHandler = async (req, res, next) => {
+    const product = await Product.find()
+
+    res.json({ product })
 }
 
-export const createProduct: RequestHandler = (req, res, next) => {
+export const createProduct: RequestHandler = async (req, res, next) => {
     const { name } = req.body as ProductDto
-    const newProduct = new Product(Math.random().toString(), name)
-    products.push(newProduct)
+    // const newProduct = new Product(Math.random().toString(), name)
+    const newProduct = new Product({ name: "Test product" })
+    await newProduct.save()
+    // products.push(newProduct)
 
     res.status(201).json({
-        message: "Created new product",
+        message: "Created a new product",
         newProduct
     })
 }
 
-export const updateProduct: RequestHandler<{ id: string }> = (req, res, next) => {
-    const productId = req.params.id
-    const productIndex = products.findIndex(p => p.id === productId)
+// export const updateProduct: RequestHandler<{ id: string }> = (req, res, next) => {
+//     const productId = req.params.id
+//     const productIndex = products.findIndex(p => p.id === productId)
 
-    if (productIndex < 0) {
-        let err = new Error("Cannot find a product...")
-        res.status(400)
-        next(err)
+//     if (productIndex < 0) {
+//         let err = new Error("Cannot find a product...")
+//         res.status(400)
+//         next(err)
 
-        return
-    }
+//         return
+//     }
 
-    const { name: productName } = req.body as ProductDTO
-    products[productIndex] = new Product(products[productIndex].id, productName)
+//     const { name: productName } = req.body as ProductDTO
+//     products[productIndex] = new Product(products[productIndex].id, productName)
 
-    res.json({
-        message: "Product updated!",
-        updatedProduct: products[productIndex]
-    })
-}
+//     res.json({
+//         message: "Product updated!",
+//         updatedProduct: products[productIndex]
+//     })
+// }
 
-export const deleteProduct: RequestHandler<{ id: string }> = (req, res, next) => {
-    const productId = req.params.id
-    const productIndex = products.findIndex(p => p.id === productId)
+// export const deleteProduct: RequestHandler<{ id: string }> = (req, res, next) => {
+//     const productId = req.params.id
+//     const productIndex = products.findIndex(p => p.id === productId)
 
-    if (productIndex < 0) {
-        let err = new Error("Cannot find a product... it is probably already deleted!")
-        res.status(400)
-        next(err)
+//     if (productIndex < 0) {
+//         let err = new Error("Cannot find a product... it is probably already deleted!")
+//         res.status(400)
+//         next(err)
 
-        return
-    }
+//         return
+//     }
 
-    products.splice(productIndex, 1)
-    res.status(200).json({
-        message: "Product deleted!",
-        products
-    })
-}
+//     products.splice(productIndex, 1)
+//     res.status(200).json({
+//         message: "Product deleted!",
+//         products
+//     })
+// }
