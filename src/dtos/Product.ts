43153947eced480924f72c680d639ad31970a615
Tomas Kaiser <tomas.kaiser06@gmail.com>
2023-12-03 21:21:26 +0000

    Add more fields for ProductDTO

--- src/dtos/Product.ts
+++ src/dtos/Product.ts
@@ -1,3 +1,13 @@
 export default interface ProductDTO {
-    name: string
+    name: string;
+    carbohydrate: string;
+    energyKj: string;
+    energyKcal: string;
+    fat: string;
+    saturatesFat: string;
+    fiber: string;
+    protein: string;
+    salt: string;
+    suger: string;
+    qrCode?: string;
 }
