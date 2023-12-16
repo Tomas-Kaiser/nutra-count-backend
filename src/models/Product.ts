7bb866e87f323c1a816001dc4b0c379622c658b1
Tomas Kaiser <tomas.kaiser06@gmail.com>
2023-12-16 18:01:50 +0000

    Update model minlength validation

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -17,16 +17,16 @@ interface Product {
 
 const productSchema = new Schema<Product>({
     name: { type: String, required: true, minlength: 3, maxlength: 30 },
-    carbohydrate: { type: String, required: true, minlength: 3, maxlength: 4 },
-    energyKj: { type: String, required: true, minlength: 3, maxlength: 4 },
-    energyKcal: { type: String, required: true, minlength: 3, maxlength: 4 },
-    fat: { type: String, required: true, minlength: 3, maxlength: 4 },
-    saturatesFat: { type: String, required: true, minlength: 3, maxlength: 4 },
-    fiber: { type: String, required: true, minlength: 3, maxlength: 4 },
-    protein: { type: String, required: true, minlength: 3, maxlength: 4 },
-    salt: { type: String, required: true, minlength: 3, maxlength: 4 },
-    suger: { type: String, required: true, minlength: 3, maxlength: 4 },
-    barCode: { type: String, required: true, minlength: 3, maxlength: 14 }
+    carbohydrate: { type: String, required: true, minlength: 1, maxlength: 4 },
+    energyKj: { type: String, required: true, minlength: 1, maxlength: 4 },
+    energyKcal: { type: String, required: true, minlength: 1, maxlength: 4 },
+    fat: { type: String, required: true, minlength: 1, maxlength: 4 },
+    saturatesFat: { type: String, required: true, minlength: 1, maxlength: 4 },
+    fiber: { type: String, required: true, minlength: 1, maxlength: 4 },
+    protein: { type: String, required: true, minlength: 1, maxlength: 4 },
+    salt: { type: String, required: true, minlength: 1, maxlength: 4 },
+    suger: { type: String, required: true, minlength: 1, maxlength: 4 },
+    barCode: { type: String, required: true, minlength: 10, maxlength: 14 }
 });
 
 export const Product = model<Product>('Product', productSchema);
