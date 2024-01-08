138ab85d0852e09bedc0601d9af6679656d76eed
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-01-08 22:07:51 +0000

    Print out env when connecting to mongoDB

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI as string)
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        console.log(`MongoDB Connected: ${conn.connection.host} in ${process.env.NODE_ENV} environment.`)
     } catch (error) {
         console.log("DB connection failed...")
         console.log(error)
